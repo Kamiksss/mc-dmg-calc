b9812fb335eade8e2653360bb70ec29d82086a89
Kamiksss <blue.kamlotov@gmail.com>
2023-01-14 12:41:18 +0100

    just better formating

--- dmg.cpp
+++ dmg.cpp
@@ -1,62 +1,69 @@
-//still testing 
+// still testing
 
 #include <iostream>
 using namespace std;
 
-bool if_armor(){
-    Pytanie: 
+bool if_armor()
+{
+Pytanie:
     string armor;
-    cout<<"Czy masz zbroję? Y/N \n";
+    cout << "Czy masz zbroję? Y/N \n";
     cin >> armor;
-    if(armor == "Y" || armor == "y")
+    if (armor == "Y" || armor == "y")
         return true;
-    else if(armor == "N" || armor == "n")
+    else if (armor == "N" || armor == "n")
         return false;
-    else{
-        cout<<"Przepraszam nie rozumiem, ponawianie pytania... \n";
+    else
+    {
+        cout << "Przepraszam nie rozumiem, ponawianie pytania... \n";
         goto Pytanie;
-        }
-    
-}
-
-int if_ench(string ask, int level){
-    Pytanie: 
-    cout<<"Czy twoja zbroja ma enchant Protection? Y/N \n";
-    cin >> ask;
-    if(ask == "Y" || ask == "y"){
-        level = 0;
-        cout<<"Podaj poziom enchantu Protection \n";
-        cin>>level;
-        return level;
-        }
-    else if(ask == "N" || ask == "n"){
-        return 0;
-        }
-    else{
-        cout<<"Przepraszam nie rozumiem, ponawianie pytania... \n";
-        goto Pytanie;
-        }
-
-}
-
-
-int main(){
-    cout<<"Working in progress..."<<endl;
-
-       string ask;
-       int level=0;
-        
-
-    cout<<"Pytania o zbroje: \n"<<endl;
-    if(if_armor()== false){
-        cout<<"Nie masz zbroi, program na razie na tym się kończy";
-    
     }
-    else{
+}
+
+int if_ench(string ask, int level)
+{
+Pytanie:
+    cout << "Czy twoja zbroja ma enchant Protection? Y/N \n";
+    cin >> ask;
+    if (ask == "Y" || ask == "y")
+    {
+        level = 0;
+        cout << "Podaj poziom enchantu Protection \n";
+        cin >> level;
+        return level;
+    }
+    else if (ask == "N" || ask == "n")
+    {
+        return 0;
+    }
+    else
+    {
+        cout << "Przepraszam nie rozumiem, ponawianie pytania... \n";
+        goto Pytanie;
+    }
+}
+
+int main()
+{
+    cout << "Working in progress..." << endl;
+
+    string ask;
+    int level = 0;
+
+    cout << "Pytania o zbroje: \n"
+         << endl;
+    if (if_armor() == false)
+    {
+        cout << "Nie masz zbroi, program na razie na tym się kończy";
+    }
+    else
+    {
         level = if_ench(ask, level);
-            if(level == 0){
-                cout<<"Nie masz prota";
-            } else
-        cout<<"Poziom prota to "<< level;
+        if (level == 0)
+        {
+            cout << "Nie masz prota";
+        }
+        else
+            cout << "Poziom prota to " << level;
     }
 }
