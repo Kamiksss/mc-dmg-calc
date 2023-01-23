0e2ba3ba9aca5ffd6ee6139d6aa093adf357b261
Kamiksss <blue.kamlotov@gmail.com>
2023-01-23 20:16:37 +0100

    Translation

--- dmg.cpp
+++ dmg.cpp
@@ -5,34 +5,36 @@ using namespace std;
 
 int sword(int type)
 {
-    cout << "Wybierz swój miecz" << endl
+    cout << "Sword: \n"
+         << endl;
+    cout << "Choose your sword" << endl
          << "1.Wodeen 2.Stone 3.Iron 4.Gold 5.Diamond 6.Netherite" << endl;
     cin >> type;
     switch (type)
     {
     case 1:
-        cout << "Wybrałeś drewniany miecz";
+        cout << "You have choosen wooden sword";
         return 1;
         break;
     case 2:
-        cout << "Wybrałeś kamienny miecz";
+        cout << "You have choosen stone sword";
         return 2;
         break;
     case 3:
-        cout << "Wybrałeś żelazny miecz";
+        cout << "You have choosen iron sword";
 
         return 3;
         break;
     case 4:
-        cout << "Wybrałeś złoty miecz";
+        cout << "You have choosen gold sword";
         return 4;
         break;
     case 5:
-        cout << "Wybrałeś diamentowy miecz";
+        cout << "You have choosen diamond sword";
         return 5;
         break;
     case 6:
-        cout << "Wybrałeś netherite miecz";
+        cout << "You have choosen netherite sword";
         return 6;
         break;
     }
@@ -41,16 +43,17 @@ int sword(int type)
 int if_potion(int level)
 {
 Pytanie:
-    cout << "Podaj poziom effectu strength który posiadasz (wpisz zero jeśli brak)\n";
+    cout << endl
+         << "Provide your strength effect level\n";
     cin >> level;
     if (level < 0)
     {
-        cout << "Nie rozumiem, ponawiam pytanie\n";
+        cout << "I don't get it, asking again...\n";
         goto Pytanie;
     }
     else
     {
-        cout << "Wybrałeś poziom effectu strength: " << level << "\n";
+        cout << "You have choosen level: " << level << "\n";
         return level;
     }
 }
@@ -59,7 +62,7 @@ bool if_armor()
 {
 Pytanie:
     string armor;
-    cout << "Czy przeciwnik ma zbroję? Y/N \n";
+    cout << "Does your enemy wear armor? Y/N\n";
     cin >> armor;
     if (armor == "Y" || armor == "y")
         return true;
@@ -67,7 +70,7 @@ Pytanie:
         return false;
     else
     {
-        cout << "Przepraszam nie rozumiem, ponawianie pytania... \n";
+        cout << "I don't get it, asking again...\n";
         goto Pytanie;
     }
 }
@@ -75,12 +78,12 @@ Pytanie:
 int if_ench(string ask, int level)
 {
 Pytanie:
-    cout << "Czy zbroja przeciwnika ma enchant Protection? Y/N \n";
+    cout << "Does your enemy's armor have protection enchant Y/N \n";
     cin >> ask;
     if (ask == "Y" || ask == "y")
     {
         level = 0;
-        cout << "Podaj poziom enchantu Protection \n";
+        cout << "Provide protection level\n";
         cin >> level;
         return level;
     }
@@ -90,42 +93,36 @@ Pytanie:
     }
     else
     {
-        cout << "Przepraszam nie rozumiem, ponawianie pytania... \n";
+        cout << "I don't get it, asking again...\n";
         goto Pytanie;
     }
 }
 
 int main()
 {
-    cout << "Working in progress..." << endl;
-
     string ask_armor;
     int level_prot = 0;
     int sword_type = 0;
     int strength_value = 0;
 
-    cout << "Pytania o zbroje: \n"
-         << endl;
+    cout << "Armor: \n";
     if (if_armor() == false)
     {
-        cout << "Brak zbroi\n";
+        cout << "You choose no armor\n";
     }
     else
     {
         level_prot = if_ench(ask_armor, level_prot);
         if (level_prot == 0)
         {
-            cout << "Brak prota\n";
+            cout << "You don't have a protection enchantment\n";
         }
         else
-            cout << "Poziom prota to " << level_prot << endl;
+            cout << "You protection enchantment level is " << level_prot << endl;
     }
 
-    cout << "Pytania o miecz \n";
     sword_type = sword(sword_type);
-    cout << sword_type << endl;
     strength_value = if_potion(strength_value);
-    cout << strength_value << endl;
 
     return 0;
 }
